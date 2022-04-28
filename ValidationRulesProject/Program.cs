e3469e67916e84c088135c3ff43098bdd73828d5
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-04-28 08:48:12 +0200

    Kiinduló projekt

---
+++ ValidationRulesProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValidationRulesProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
